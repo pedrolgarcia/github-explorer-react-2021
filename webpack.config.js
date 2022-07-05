bb141d060bd98f10effebeb9669844310eeb1381
Pedro Lopes <pedrolopesgrc@gmail.com>
2022-07-04 22:44:18 -0300

    feat: configure render, webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
-const path = require("path")
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  mode: "development",
   entry: path.resolve(__dirname, "src", "index.jsx"),
   output: {
     path: path.resolve(__dirname, "dist"),
@@ -9,9 +11,17 @@ module.exports = {
   resolve: {
     extensions: [".js", ".jsx"]
   },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public")
+    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html")
+    })
+  ],
   module: {
-    rules: [
-      { test: /\.jsx$/, exclude: /node_modules/, use: "babel-loader" }
-    ]
+    rules: [{ test: /\.jsx$/, exclude: /node_modules/, use: "babel-loader" }]
   }
-}
+};
